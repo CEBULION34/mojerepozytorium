1c28ea03fd099b651939b45b599dea4946c19495
CEBULION34 <zarazem34@gmail.com>
2018-04-07 20:41:07 +0200

    dodanie plikow do zdalnego repozytorium

---
+++ Test.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Test {
+	
+	
+	public static void main (String [] args0) {
+		
+		//Collection<Person> list = new ArrayList<Person>();
+		
+		Student student = new Student("PanTadeusz", "woda");
+		
+		//System.out.println(list.add(student));
+		
+		System.out.println(student.loginPerson());
+		
+		
+		
+		
+		
+	}
+
+}
